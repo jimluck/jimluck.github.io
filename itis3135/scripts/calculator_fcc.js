70becfdbcecb7ccf4c8ae786ff2e55043ca14db4
Jimmy Kalume Lubeta <klubeta@uncc.edu>
2024-05-03 18:56:48 -0400

    calculator_fccc update

---
+++ itis3135/scripts/calculator_fcc.js
@@ -0,0 +1,62 @@
+const calculator = document.querySelector('.calculator');
+const keys = calculator.querySelector('.calculator__keys');
+const display = calculator.querySelector('.calculator__display');
+
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        const key = e.target;
+        const action = key.dataset.action;
+        const keyContent = key.textContent;
+        const displayedNum = display.textContent;
+
+        if (!action) {
+            if (displayedNum === '0') {
+                display.textContent = keyContent;
+            } else {
+                display.textContent = displayedNum + keyContent;
+            }
+        }
+
+        if (action === 'decimal') {
+            if (!displayedNum.includes('.')) {
+                display.textContent = displayedNum + '.';
+            }
+        }
+
+        if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
+            key.classList.add('is-depressed');
+            calculator.dataset.previousKeyType = 'operator';
+            calculator.dataset.firstValue = displayedNum;
+            calculator.dataset.operator = action;
+        }
+
+        if (action === 'clear') {
+            display.textContent = '0';
+        }
+
+        if (action === 'calculate') {
+            const firstValue = calculator.dataset.firstValue;
+            const operator = calculator.dataset.operator;
+            const secondValue = displayedNum;
+            display.textContent = calculate(firstValue, operator, secondValue);
+        }
+
+        Array.from(keys.children).forEach(k => k.classList.remove('is-depressed'));
+    }
+});
+
+function calculate(n1, operator, n2) {
+    let result = '';
+
+    if (operator === 'add') {
+        result = parseFloat(n1) + parseFloat(n2);
+    } else if (operator === 'subtract') {
+        result = parseFloat(n1) - parseFloat(n2);
+    } else if (operator === 'multiply') {
+        result = parseFloat(n1) * parseFloat(n2);
+    } else if (operator === 'divide') {
+        result = parseFloat(n1) / parseFloat(n2);
+    }
+
+    return result;
+}
