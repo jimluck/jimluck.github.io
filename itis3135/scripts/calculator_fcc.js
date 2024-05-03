df9e14fb0c0cb5f6c7667b2dff0e41a27f5e051e
Jimmy Kalume Lubeta <klubeta@uncc.edu>
2024-05-03 19:17:45 -0400

    calculator_fcc update

--- itis3135/scripts/calculator_fcc.js
+++ itis3135/scripts/calculator_fcc.js
@@ -1,16 +1,19 @@
-const calculator = document.querySelector('.calculator');
-const keys = calculator.querySelector('.calculator__keys');
-const display = calculator.querySelector('.calculator__display');
+document.addEventListener("DOMContentLoaded", function() {
+    const calculator = document.querySelector('.calculator');
+    const display = calculator.querySelector('.calculator__display');
+    const keys = calculator.querySelector('.calculator__keys');
 
-keys.addEventListener('click', e => {
-    if (e.target.matches('button')) {
+    keys.addEventListener('click', e => {
+        if (!e.target.matches('button')) return;
+
         const key = e.target;
         const action = key.dataset.action;
         const keyContent = key.textContent;
         const displayedNum = display.textContent;
+        let previousKeyType = calculator.dataset.previousKeyType;
 
         if (!action) {
-            if (displayedNum === '0') {
+            if (displayedNum === '0' || previousKeyType === 'operator' || previousKeyType === 'calculate') {
                 display.textContent = keyContent;
             } else {
                 display.textContent = displayedNum + keyContent;
@@ -20,6 +23,8 @@ keys.addEventListener('click', e => {
         if (action === 'decimal') {
             if (!displayedNum.includes('.')) {
                 display.textContent = displayedNum + '.';
+            } else if (previousKeyType === 'operator' || previousKeyType === 'calculate') {
+                display.textContent = '0.';
             }
         }
 
@@ -32,31 +37,33 @@ keys.addEventListener('click', e => {
 
         if (action === 'clear') {
             display.textContent = '0';
+            calculator.dataset.previousKeyType = 'clear';
         }
 
         if (action === 'calculate') {
-            const firstValue = calculator.dataset.firstValue;
+            let firstValue = calculator.dataset.firstValue;
             const operator = calculator.dataset.operator;
-            const secondValue = displayedNum;
-            display.textContent = calculate(firstValue, operator, secondValue);
+            let secondValue = displayedNum;
+            if (firstValue) {
+                if (previousKeyType === 'calculate') {
+                    firstValue = displayedNum;
+                    secondValue = calculator.dataset.modValue;
+                }
+                display.textContent = calculate(firstValue, operator, secondValue);
+            }
+            calculator.dataset.modValue = secondValue;
+            calculator.dataset.previousKeyType = 'calculate';
         }
 
         Array.from(keys.children).forEach(k => k.classList.remove('is-depressed'));
+    });
+
+    function calculate(n1, operator, n2) {
+        const firstNum = parseFloat(n1);
+        const secondNum = parseFloat(n2);
+        if (operator === 'add') return firstNum + secondNum;
+        if (operator === 'subtract') return firstNum - secondNum;
+        if (operator === 'multiply') return firstNum * secondNum;
+        if (operator === 'divide') return firstNum / secondNum;
     }
 });
-
-function calculate(n1, operator, n2) {
-    let result = '';
-
-    if (operator === 'add') {
-        result = parseFloat(n1) + parseFloat(n2);
-    } else if (operator === 'subtract') {
-        result = parseFloat(n1) - parseFloat(n2);
-    } else if (operator === 'multiply') {
-        result = parseFloat(n1) * parseFloat(n2);
-    } else if (operator === 'divide') {
-        result = parseFloat(n1) / parseFloat(n2);
-    }
-
-    return result;
-}
