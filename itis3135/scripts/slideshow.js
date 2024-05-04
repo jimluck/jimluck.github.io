9530263be4851d2afbc8403db08dd2235907b29c
Jimmy Kalume Lubeta <klubeta@uncc.edu>
2024-05-04 00:01:57 -0400

    Changes

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
     const images = [
-        { src: 'path_to_jug.jpg', caption: 'J for Jug' },
-        { src: 'path_to_icecream.jpg', caption: 'I for Ice cream' },
-        { src: 'path_to_moon.jpg', caption: 'First M for Moon' },
-        { src: 'path_to_mirror.jpg', caption: 'Second M for Mirror' },
-        { src: 'path_to_yacht.jpg', caption: 'Y for Yacht' },
-        { src: 'path_to_lion.jpg', caption: 'L for Lion' }, // Updated to Lion
-        { src: 'path_to_umbrella.jpg', caption: 'U for Umbrella' },
-        { src: 'path_to_book.jpg', caption: 'B for Book' },
-        { src: 'path_to_elephant.jpg', caption: 'E for Elephant' },
-        { src: 'path_to_tree.jpg', caption: 'T for Tree' },
-        { src: 'path_to_apple.jpg', caption: 'A for Apple' }
+        { src: '../images/jug.jpg', caption: 'J for Jug' },  // Update as per actual image name
+        { src: '../images/icecream.jpg', caption: 'I for Ice cream' },  // Update as per actual image name
+        { src: '../images/moon.jpg', caption: 'First M for Moon' },
+        { src: '../images/mirror.jpg', caption: 'Second M for Mirror' },  // Update as per actual image name
+        { src: '../images/yacht.jpg', caption: 'Y for Yacht' },
+        { src: '../images/lion.jpg', caption: 'L for Lion' },  // Update as per actual image name
+        { src: '../images/umbrella.jpg', caption: 'U for Umbrella' },
+        { src: '../images/book.jpg', caption: 'B for Book' },  // Update as per actual image name
+        { src: '../images/elephant.jpg', caption: 'E for Elephant' },  // Update as per actual image name
+        { src: '../images/tree.jpg', caption: 'T for Tree' },
+        { src: '../images/apple.jpg', caption: 'A for Apple' }  // Update as per actual image name
     ];
 
     let currentImage = 0;
